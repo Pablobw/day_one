47ca877eb5c698e8cef4e849f8f19d55a43c74c1
silverFox78 <sbarrera@gmail.com>
2016-12-09 21:52:02 -0300

    prompt y if

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ alert("hola mundo");
 console.log("Donde saldra esto???");
 var x = 1;
 var y = 5;
+
+var valor = prompt("Ingrese un numero");
+if (valor > 5) {
+    alert("El valor es mayor de 5");
+} else {
+    alert("El valor es menor de 5");
+}
